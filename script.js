e157627337c01adeefba336cfd0d1bc6e89acd3f
Thiago Rocha <thiagord2016@gmail.com>
2021-10-28 17:38:36 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -9,11 +9,24 @@ const links = {
 function changeSocialMediaLinks() {
     for (let li of socialLinks.children) {
         const social = li.getAttribute('class')
-        li.children[0].href = "https://www." + social + ".com/" + links.social
         li.children[0].href = `https://www.${social}.com/${links[social]}`
-
-        // alert(li.children[0].href)
     }
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${links.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPic.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
